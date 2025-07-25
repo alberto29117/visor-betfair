36ccd65ce28440936a7b777bf872e6612689590c
alberto29117 <alberfp92@gmail.com>
2025-07-25 17:15:04 +0200

    fetch-races.js

--- netlify/functions/fetch-races.js
+++ netlify/functions/fetch-races.js
@@ -23,6 +23,8 @@ exports.handler = async function (event, context) {
         'X-Authentication': sessionToken,
         'Content-Type': 'application/json',
         'Accept': 'application/json',
+        // AÑADIMOS ESTA LÍNEA PARA EVITAR EL BLOQUEO DE CLOUDFLARE
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
       },
       body: JSON.stringify(betfairBody),
     });
